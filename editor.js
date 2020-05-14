ff278c2ef3c02c664e3cc08d00f7c3c505992e55
RuanSena <ruansenadev@gmail.com>
2020-05-14 17:26:18 -0300

    filter apply

--- editor.js
+++ editor.js
@@ -18,18 +18,31 @@ const editor = {
         await page.goto(imgPath, {timeout: 10000})
         await page.waitFor('img')
     },
-    filter: (options = defaults.filter) => {
-        
+    filter: (options = {blur: '', brightness: 1, contrast: 1, drop_shadow: '', grayscale: 0, hue_rotate: '', invert: 0, opacity: 1, saturate: 1, sepia: 0}) => {
+        let functions = []
+        for(let [prop, val] of Object.entries(options)) {
+            prop = prop.replace('_', '-')
+            if(val !== defaults.filter[prop]) {
+                functions.push(`${prop}(${val})`)
+            }
+        }
+        if(functions.length) {
+            fx.filter = `${functions.join(' ')}`
+        } else {
+            fx.filter = 'none'
+        }
     },
     apply: async() => {
-        await page.evaluate(() => {
+        await page.evaluate((fx) => {
             img = document.querySelector('img')
-
-        })
+            for(let prop in fx) {
+                img.style[prop] = fx[prop]
+            }
+        }, fx)
     },
     save: async(imgName='screenshot') => {
         fs.mkdirSync(path.join(__dirname, 'images'), {recursive: true})
-        await page.screenshot({path: path.join(__dirname, 'images', `${imgName}.jpeg`)})
+        await page.screenshot({path: path.join(__dirname, 'images', `${imgName}.png`)})
         browser.close()
     }
 }
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const editor = require('./editor')
 
 editor.open('https://www.colorzilla.com/images/colorzilla-for-chrome.png')
     .then(() => {
-        editor.save()
+        // define styles effects
+        editor.filter({blur: '4px', sepia: 0.4})
     })
     .catch(console.error)
+    .finally(() => {
+        // apply styles
+        editor.apply()
+        // screenshots the page
+        editor.save('filter')
+    })
