512cef4e3c442ed23338ef2cc839c7983ad38a58
RuanSena <ruansenadev@gmail.com>
2020-05-14 16:10:51 -0300

    defaults

--- editor.js
+++ editor.js
@@ -5,18 +5,31 @@ const path = require('path')
 let browser = null
 let page = null
 let img = null
+let fx = {
 
+}
+const defaults = {
+    filter: {blur: '', brightness: 1, contrast: 1, 'drop-shadow': '', grayscale: 0, 'hue-rotate': '', invert: 0, opacity: 1, saturate: 1, sepia: 0}
+}
 const editor = {
     open: async(imgPath='') => {
         browser = await pupp.launch({headless: false});
         [page] = await browser.pages();
         await page.goto(imgPath, {timeout: 10000})
         await page.waitFor('img')
-        img = await page.$('img')
     },
-    save: async() => {
+    filter: (options = defaults.filter) => {
+        
+    },
+    apply: async() => {
+        await page.evaluate(() => {
+            img = document.querySelector('img')
+
+        })
+    },
+    save: async(imgName='screenshot') => {
         fs.mkdirSync(path.join(__dirname, 'images'), {recursive: true})
-        await page.screenshot({path: path.join(__dirname, 'images', 'imageScreenshot.png')})
+        await page.screenshot({path: path.join(__dirname, 'images', `${imgName}.jpeg`)})
         browser.close()
     }
 }
