432bc93c4ffca7b278c49277b984f01f823d6f4a
RuanSena <ruansenadev@gmail.com>
2020-05-17 15:13:16 -0300

    resize

--- editor.js
+++ editor.js
@@ -7,6 +7,7 @@ const LOCAL = 'localhost:8080'
 let browser = null
 let page = null
 let imgHandle = null
+let ops = []
 let styleFX = {
 
 }
@@ -32,11 +33,38 @@ const editor = {
         }
         if(functions.length) {
             styleFX.filter = `${functions.join(' ')}`
-        } else {
-            styleFX.filter = 'none'
+            ops.push('filter')
         }
     },
+    resize: async(w=0, h=0, scale=1, ratio='initial') => {
+        let {width, height} = await imgHandle.boundingBox()
+        let newWidth = w || width
+        let newHeight = h || height
+
+        if(scale !== 1 && scale) styleFX.transform = styleFX.transform ? styleFX.transform + ` scale(${scale})` : `scale(${scale})`
+
+        if(ratio !== 'initial') {
+            styleFX.objectFit = 'cover';
+            // cut aspect ratio
+            switch (ratio) {
+                case '1:1':
+                    if(newWidth > newHeight) newWidth = newHeight
+                    else newHeight = newWidth
+                    break
+                default:
+                    // ratio width must be greater than height (3:2, 4:3, 16:9)
+                    ratio = ratio.split(':')
+                    newHeight = ((newWidth * Number(ratio[1])) / Number(ratio[0])).toFixed(2)
+                    break
+            }
+        }
+        
+        if(newWidth !== width) styleFX.width = newWidth + 'px'
+        if(newHeight !== height) styleFX.height = newHeight + 'px'
+        ops.push('resize')
+    },
     apply: async() => {
+        console.log(styleFX)
         await imgHandle.evaluate((img, fx) => {
             img = document.querySelector('img')
             for(let prop in fx) {
@@ -46,7 +74,7 @@ const editor = {
     },
     save: async(imgName='screenshot') => {
         fs.mkdirSync(path.join(__dirname, 'images'), {recursive: true})
-        imgName = `${imgName}_${Object.keys(styleFX).join('_')}.png`
+        imgName = `${imgName}_${ops.join('_')}.png`
         await imgHandle.screenshot({path: path.join(__dirname, 'images', imgName), type: 'png', omitBackground: true})
         console.log('+ ' + imgName)
     },
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const editor = require('./editor')
 
 editor.open("https://i1.sndcdn.com/artworks-000644480044-6z10uu-t500x500.jpg")
-    .then(() => {
+    .then(async () => {
         // define styles effects
-        editor.filter({blur: '2px', sepia: 0.6})
-
+        // editor.filter({blur: '2px', sepia: 0.6})
+        await editor.resize(64)
         // apply styles
         editor.apply()
         // screenshots the image
-        editor.save('filter')
+        editor.save()
     })
     .catch(console.error)
     .finally(editor.close)
